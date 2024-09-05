9d45782ed84aa3c42af313a69bed3738abbd35af
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-09-05 09:17:36 -0400

    init spring

---
+++ src/main/java/bo/edu/ucb/syntax_flavor_backend/SyntaxFlavorBackendApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.syntax_flavor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntaxFlavorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntaxFlavorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/syntax_flavor_backend/SyntaxFlavorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.syntax_flavor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntaxFlavorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
