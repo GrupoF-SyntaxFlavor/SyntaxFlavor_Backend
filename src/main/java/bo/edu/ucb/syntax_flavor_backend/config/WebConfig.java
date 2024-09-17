dda60ffcfd5d2d806a445eb5e74e607503775ea5
dianamongar <dianaaneliz@gmail.com>
2024-09-17 19:57:38 -0400

    configuración de error de cors

---
+++ src/main/java/bo/edu/ucb/syntax_flavor_backend/config/WebConfig.java
@@ -0,0 +1,22 @@
+package bo.edu.ucb.syntax_flavor_backend.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //Esta clase es la que se encarga de la configuracion de la conexion entre el front end y el back end
+    //Configura el CORS entre los puertos 8080 y 8081
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
