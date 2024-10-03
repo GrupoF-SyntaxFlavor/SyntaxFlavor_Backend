57f0b2c3efe0dc76444fa65343325fd3ece743ce
dianamongar <dianaaneliz@gmail.com>
2024-10-03 10:51:34 -0400

    fixing endpoint signup customers

--- src/main/java/bo/edu/ucb/syntax_flavor_backend/user/bl/CustomerBL.java
+++ src/main/java/bo/edu/ucb/syntax_flavor_backend/user/bl/CustomerBL.java
@@ -5,9 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import bo.edu.ucb.syntax_flavor_backend.user.dto.CustomerDTO;
 import bo.edu.ucb.syntax_flavor_backend.user.dto.CustomerRequestDTO;
+import bo.edu.ucb.syntax_flavor_backend.user.dto.UserDTO;
 import bo.edu.ucb.syntax_flavor_backend.user.entity.Customer;
+import bo.edu.ucb.syntax_flavor_backend.user.entity.User;
 import bo.edu.ucb.syntax_flavor_backend.user.repository.CustomerRepository;
+import bo.edu.ucb.syntax_flavor_backend.user.repository.UserRepository;
 
 @Component
 public class CustomerBL {
@@ -16,6 +20,14 @@ public class CustomerBL {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public CustomerBL(CustomerRepository customerRepository, UserRepository userRepository) {
+        this.customerRepository = customerRepository;
+        this.userRepository = userRepository;
+    }
 
     public Customer findCustomerById(Integer customerId) throws RuntimeException{
         LOGGER.info("Finding customer by id: {}", customerId);
@@ -43,6 +55,24 @@ public class CustomerBL {
         catch(Exception e) {
             LOGGER.error("Error updating customer data: {}", e.getMessage());
             throw new RuntimeException("Error updating customer data: " + e.getMessage());
+        }
+        
+    }
+
+    public CustomerDTO createCustomer(UserDTO user, String nit, String billName) {
+        LOGGER.info("Creating customer with user: {}", user);
+        try{
+            Customer newCustomer = new Customer();
+            User localUser = userRepository.findById(user.getUserId()).orElseThrow(() -> new RuntimeException("No user with provided ID was found"));
+            newCustomer.setUsersId(localUser);
+            newCustomer.setBillName(billName);
+            newCustomer.setNit(nit);
+            Customer customer = customerRepository.save(newCustomer);
+            return new CustomerDTO(customer);
+        }
+        catch(Exception e) {
+            LOGGER.error("Error creating customer: {}", e.getMessage());
+            throw new RuntimeException("Error creating customer: " + e.getMessage());
         }
         
     }
