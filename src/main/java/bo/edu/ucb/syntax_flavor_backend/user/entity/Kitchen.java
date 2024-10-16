7e92f9fef4bb29c6a77b43212125f97fc9616c44
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-10-16 08:22:14 -0400

    cocinas en DB

---
+++ src/main/java/bo/edu/ucb/syntax_flavor_backend/user/entity/Kitchen.java
@@ -0,0 +1,57 @@
+package bo.edu.ucb.syntax_flavor_backend.user.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "kitchens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Kitchen implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "location")
+    private String location;
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = new Date();
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt = new Date();
+    @JoinColumn(name = "users_id", referencedColumnName = "id")
+    @ManyToOne
+    private User usersId;
+
+    @Override
+    public String toString(){
+        return "Kitchen{" +
+                "id=" + id +
+                ", location='" + location + '\'' +
+                ", usersId=" + usersId +
+                '}';
+    }
+
+}
