d0d3c225e9a6d950863a5dee482f4eb7384498cb
dianamongar <dianaaneliz@gmail.com>
2024-10-16 11:01:27 -0400

    desbloqueo swagger

--- src/main/java/bo/edu/ucb/syntax_flavor_backend/config/SecurityConfig.java
+++ src/main/java/bo/edu/ucb/syntax_flavor_backend/config/SecurityConfig.java
@@ -20,7 +20,8 @@ class SecurityConfig {
                 .and()
                 .authorizeHttpRequests(c ->
                         c
-                        .requestMatchers("/api/v1/public/**").permitAll()  // Rutas públicas
+                        .requestMatchers("/api/v1/public/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()  // Rutas públicas
                         .anyRequest().authenticated()  // Todas las demás rutas requieren autenticación
                 )
                 .sessionManagement(c ->
