363d676f16e8d62b801d890f6fe674f141a0339a
dianamongar <dianaaneliz@gmail.com>
2024-10-20 22:13:43 -0400

    cambios filtrado ordenes

--- src/main/java/bo/edu/ucb/syntax_flavor_backend/order/repository/OrderRepository.java
+++ src/main/java/bo/edu/ucb/syntax_flavor_backend/order/repository/OrderRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import bo.edu.ucb.syntax_flavor_backend.order.entity.Order;
 
@@ -34,5 +33,9 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     //filtrar por status
     Page<Order> findAllByOrderTimestampBetweenAndStatusOrderByOrderTimestampAsc(LocalDateTime startOfDay, LocalDateTime endOfDay, String status, Pageable pageable);
+
+    //filtrar por status, fechas y ordenar por fecha
+    @Query("SELECT o FROM Order o WHERE o.orderTimestamp BETWEEN :minDate AND :maxDate AND o.status = :status")
+    Page<Order> findByMinDateBetweenMaxDateAndStatusByNameAsc(String status, LocalDateTime minDate, LocalDateTime maxDate, Pageable pageable);
 
 }
