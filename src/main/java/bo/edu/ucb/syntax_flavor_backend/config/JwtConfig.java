c8d60c7f9ff031976e6d1d4dd0b648f348eddd57
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-11-07 10:20:38 -0400

    corrigiendo el FIXME en orders, mover lógica a BL

--- src/main/java/bo/edu/ucb/syntax_flavor_backend/config/JwtConfig.java
+++ src/main/java/bo/edu/ucb/syntax_flavor_backend/config/JwtConfig.java
@@ -2,18 +2,21 @@ package bo.edu.ucb.syntax_flavor_backend.config;
 
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JwtConfig {
 
+    @Value("${spring.keycloak.auth-server-url}")
+    private String keycloakUrl;
+
     @Bean
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder
                 .withJwkSetUri(
-                        "http://syntaxflavor_keycloak:8080/realms/syntaxflavor_users/protocol/openid-connect/certs")
-                .build(); // FIXME: Estas URL deberían ser configurables desde un archivo de propiedades o
-                          // env
+                        keycloakUrl+"/realms/syntaxflavor_users/protocol/openid-connect/certs")
+                .build();
     }
 }
